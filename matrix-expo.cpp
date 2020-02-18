fc28a3d462241f4b15ca5b6db10bbd3b4366ecb9
namank63 <48546593+namank63@users.noreply.github.com>
2020-02-18 19:19:07 +0530

    Corrections made to code

--- matrix-expo.cpp
+++ matrix-expo.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-void multiplyMatirix(int resMatrix[2][2], int b[2][2])          // 2 Matrix Multiplication function
+void multiplyMatrix(int resMatrix[2][2], int b[2][2])          // 2 Matrix Multiplication function
 {
+    int temp[2][2];
     for(int i=0; i<2; i++)
     {
         for(int j=0; j<2; j++)
         {
             temp[i][j] = 0;
             for(int k=0; k<2 ; k++)
-            temp[i][j] += resMatrix[i][k] * b[k][j];                    
+                temp[i][j] += resMatrix[i][k] * b[k][j];
         }
     }
     for(int i=0; i<2; i++)                  // Updating the value of the constant matrix.
     {
-        for(int j=0; i<2; j++)
+        for(int j=0; j<2; j++)
         {
             resMatrix[i][j] = temp[i][j];
         }
@@ -31,23 +32,23 @@ void matrixFastExpo(int resMatrix[2][2], int n, int x, int y)
         return;     // End function call.
     }
     matrixFastExpo(resMatrix, n/2, x, y);
-    multiplyMatirix(resMatrix,resMatrix);
-    if(n % 2 != 0) multiplyMatirix(resMatrix, constantMatrix);
+    multiplyMatrix(resMatrix,resMatrix);
+    if(n % 2 != 0) multiplyMatrix(resMatrix, constantMatrix);
 }
 
-int matrixExpo(int n, int x, int y)
-{
+int matrixExpo(int n, int x, int y) {
     int resMatrix[2][2] = {{1,1},
                            {1,0}};     // The constant matrix for the fibonacci relation.
-    if(n == 1) return y;                    
-    if(n == 0) return x;                    
+    if(n == 1) return y;
+    if(n == 0) return x;
     matrixFastExpo(resMatrix, n-1, x, y);
     return (resMatrix[0][0] * y) + (resMatrix[0][1] * x);
 }
+
 int main()
 {
     int x,y,n;
     cin >> n >> x >> y;
     cout << matrixExpo(n,x,y);
-    return 0;   
+    return 0;
 }
